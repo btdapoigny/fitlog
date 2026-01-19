230288240f2884c65f347b544734fa32652f418e
Benoît Dapoigny <btdapoigny@me.com>
2026-01-19 20:04:59 +0100

    Refactor routes in a seperate file

--- src/main.jsx
+++ src/main.jsx
@@ -4,38 +4,14 @@ import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
 
 import App from './App.jsx'
-import { Dashboard } from './pages/Dashboard.jsx'
-import { WorkoutSessions } from './pages/WorkoutSessions.jsx'
-import { WorkoutTemplates } from './pages/WorkoutTemplates.jsx'
+import { routes } from './routes.jsx'
 
 import './assets/scss/bundle.scss';
 
 const router = createBrowserRouter([
   {
     element: <App />,
-    children: [
-      { 
-        path: '/', 
-        element: <Dashboard />,
-        handle: {
-          title: 'Dashboard',
-        },
-      },
-      { 
-        path: '/seances', 
-        element: <WorkoutSessions />,
-        handle: {
-          title: 'Séances',
-        },
-      },
-      { 
-        path: '/modeles', 
-        element: <WorkoutTemplates />,
-        handle: {
-          title: 'Modèles',
-        },
-      },
-    ]
+    children: routes,
   }
 ])
 
---
+++ src/routes.jsx
@@ -0,0 +1,27 @@
+import { Dashboard } from './pages/Dashboard.jsx'
+import { WorkoutSessions } from './pages/WorkoutSessions.jsx'
+import { WorkoutTemplates } from './pages/WorkoutTemplates.jsx'
+
+export const routes = [
+  {
+    path: '/', 
+    element: <Dashboard />,
+    handle: {
+      title: 'Dashboard',
+    },
+  },
+  {
+    path: '/seances', 
+    element: <WorkoutSessions />,
+    handle: {
+      title: 'Séances',
+    },
+  },
+  {
+    path: '/modeles', 
+    element: <WorkoutTemplates />,
+    handle: {
+      title: 'Modèles',
+    },
+  },
+]
