e432743545e6ba35fa5a268a1ef3fe05616f09a1
Benoît Dapoigny <btdapoigny@me.com>
2026-01-19 19:37:08 +0100

    Add router with a default layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import './assets/scss/bundle.scss';
+import { Outlet, useMatches } from 'react-router'
 
 function App() {
+  const matches = useMatches()
+
+  const pageTitle = matches[matches.length - 1].handle?.title
+
   return (
     <>
+      <header>
+        <h1>{ pageTitle }</h1>
+      </header>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,46 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+
 import App from './App.jsx'
+import { Dashboard } from './pages/Dashboard.jsx'
+import { WorkoutSessions } from './pages/WorkoutSessions.jsx'
+import { WorkoutTemplates } from './pages/WorkoutTemplates.jsx'
+
+import './assets/scss/bundle.scss';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { 
+        path: '/', 
+        element: <Dashboard />,
+        handle: {
+          title: 'Dashboard',
+        },
+      },
+      { 
+        path: '/seances', 
+        element: <WorkoutSessions />,
+        handle: {
+          title: 'Séances',
+        },
+      },
+      { 
+        path: '/modeles', 
+        element: <WorkoutTemplates />,
+        handle: {
+          title: 'Modèles',
+        },
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={ router } />
   </StrictMode>
 )
