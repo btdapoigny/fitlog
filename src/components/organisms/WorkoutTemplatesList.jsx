3ef5c5e34aa27e2eb5b53485b737d516b0a37328
Benoît Dapoigny <btdapoigny@me.com>
2026-01-25 18:29:44 +0100

    Add WorkoutTemplates layout (UI only with static data)

--- src/components/organisms/WorkoutTemplatesList.jsx
+++ src/components/organisms/WorkoutTemplatesList.jsx
@@ -1,14 +1,14 @@
 import { AddItem } from "@/components/molecules/AddItem"
 import { WorkoutTemplateItem } from "@/components/molecules/WorkoutTemplateItem"
 
-export function WorkoutTemplatesList({ title, description, list }) {
+export function WorkoutTemplatesList({ title, description, list, handleAddItem }) {
   return (
     <section className="workout-templates-list">
       <h2 className="workout-templates-list__title">{ title }</h2>
       <p className="workout-templates-list__description">{ description }</p>
       <div className="workout-templates-list__wrapper">
         { list.map(item => <WorkoutTemplateItem key={ item.id } { ...item } />) }
-        <AddItem label="Ajouter un modèle" />
+        <AddItem label="Ajouter un modèle" handleClick={ handleAddItem } />
       </div>
     </section>
   )
--- src/pages/WorkoutTemplates.jsx
+++ src/pages/WorkoutTemplates.jsx
@@ -1,3 +1,62 @@
+import { useState } from 'react'
+import { Input } from '@/components/atoms/Input'
+import { Button } from '../components/atoms/Button'
+import { WorkoutTemplatesList } from '@/components/organisms/WorkoutTemplatesList'
+import { Modal } from '@/components/organisms/Modal'
+
+import ChestImage from '@/assets/img/chest.jpg'
+import BackImage from '@/assets/img/back.jpg'
+import LegsImage from '@/assets/img/legs.jpg'
+
 export function WorkoutTemplates() {
-  
+  const [showModal, setShowModal] = useState(false)
+
+  const submitForm = (event) => {
+    event.preventDefault()
+    setShowModal(false)
+  }
+
+  const workoutTemplatesListData = {
+    title: "Mes modèles",
+    description: "Ajouter ou modifier des modèles de séances. Chaque modèle contient l’ensemble des exercices à effectuer lors d’une séance récurrente, et peut être utilisé lors de l’ajout une nouvelle séance. Utiliser des modèles permet d’avoir un meilleur suivi de la progression de semaines en semaines. Si la séance est unique et non récurrente, il est préférable de créer une séance sans utiliser de modèle.",
+    list: [
+      {
+        id: 1,
+        title: "Push",
+        uptitle: "Focus force",
+        description: "Séance push avec un focus sur la force. Muscles ciblés : Pectoraux, triceps, épaules.",
+        thumbnail: ChestImage
+      },
+      {
+        id: 2,
+        title: "Pull",
+        uptitle: "Focus grand dorsal",
+        description: "Séance pull avec un focus sur le grand dorsal. Muscles ciblés : Dos, biceps, avant-bras.",
+        thumbnail: BackImage
+      },
+      {
+        id: 3,
+        title: "Legs",
+        description: "Séance jambes globale. Muscles ciblées : Quadriceps, ischio-jambiers, fessiers.",
+        thumbnail: LegsImage
+      },
+    ]
+  }
+
+  return (
+    <>
+      <WorkoutTemplatesList { ...workoutTemplatesListData } handleAddItem={ () => setShowModal(true) } />
+      { showModal &&
+        <Modal title="Ajouter un modèle" closeModal={ () => setShowModal(!showModal) }>
+          <form onSubmit={ submitForm }>
+            <Input label="Nom du modèle" type="text" placeholder="Ex. Legs" />
+            <Input label="Titre secondaire" type="text" placeholder="Ex. Focus quadriceps" />
+            <Input label="Description" type="textarea" placeholder="Entrer la description..." />
+            <Input label="Image" type="file" placeholder="Choisir une image" />
+            <Button label="Ajouter" type="submit" />
+          </form>
+        </Modal>
+      }
+    </>
+  )
 }
