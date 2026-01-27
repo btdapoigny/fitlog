12d8cb686449c0b32eb6fd669160ba994f026f63
Benoît Dapoigny <btdapoigny@me.com>
2026-01-27 21:34:41 +0100

    Add IconButton component

---
+++ src/components/atoms/IconButton.jsx
@@ -0,0 +1,24 @@
+import { classList } from '@/utils/classList'
+
+import EditIcon from '@/assets/icons/all/edit.svg?react'
+import DeleteIcon from '@/assets/icons/all/delete.svg?react'
+import SaveIcon from '@/assets/icons/all/save.svg?react'
+
+const ICONS = {
+  edit: EditIcon,
+  delete: DeleteIcon,
+  save: SaveIcon
+}
+
+export function IconButton({ icon, handleClick, }) {
+  const Icon = ICONS[icon]
+  const isDanger = icon === 'delete'
+
+  if (!Icon) return null
+
+  return (
+    <button className={classList('icon-button', { 'icon-button--danger': isDanger })} type="button" onClick={ handleClick }>
+      <Icon />
+    </button>
+  )
+}
