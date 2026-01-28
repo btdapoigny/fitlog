1f943d402fc5290f5d9f888cea2b25cd150c4236
Benoît Dapoigny <btdapoigny@me.com>
2026-01-28 02:37:05 +0100

    Add ExercicesList component

---
+++ src/components/organisms/ExercicesList.jsx
@@ -0,0 +1,14 @@
+import { AddItem } from "@/components/molecules/AddItem"
+import { ExerciceItem } from "@/components/organisms/ExerciceItem"
+
+export function ExercicesList({ title, list, handleAddItem }) {
+  return (
+    <section className="exercices-list">
+      <h2 className="exercices-list__title">{ title }</h2>
+      <div className="exercices-list__wrapper">
+        { list.map(item => <ExerciceItem key={ item.id } fields={ item.fields } />) }
+        <AddItem label="Ajouter un exercice" handleClick={ handleAddItem } />
+      </div>
+    </section>
+  )
+}
